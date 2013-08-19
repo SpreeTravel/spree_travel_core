5948b0eb0dbae292d86959fdc1bf96abe363fd39
Pedro Quintero Rojas <pqr@uh.cu>
2013-08-19 09:47:35 -0400

    sigo moviendo cosas de lugar

--- app/overrides/change_product_listing.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_products",
-                     :name => "change_product_listing",
-                     :replace => "#products",
-                     :partial => 'spree/shared/product_listing',
-                     :disabled => false
-)
-
