b31fc150cf00706723ab8b3352d01f5264e9fe8f
Cesar Lage <kaerdsar@gmail.com>
2013-12-21 15:52:57 -0500

    agregando metodos para option_value y variant

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -4,8 +4,8 @@ Spree::OptionValue.class_eval do
     self.option_type.position
   end
 
-  def find_or_create(name, presentation, option_type)
-    option_value = OptionValue.where(:name => name).first_or_create(
+  def self.find_or_create(name, presentation, option_type)
+    option_value = self.where(:name => name).first_or_create(
         :presentation => presentation,
         :option_type_id => Spree::OptionType.find_by_name(option_type).id
     )
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Variant.class_eval do
 
   def long_sku
-    (self.option_values.order(:position).map(&:name)).join('-')
+    self.option_values.order(:position).map(&:name).join('-')
   end
 
 end
