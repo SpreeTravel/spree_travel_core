d709c0405cd2aacf8f51bfbdd3724d4f54613319
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2013-12-30 10:40:09 -0500

    automatic commit to pull changes from github

--- app/helpers/spree/application_helper_decorator.rb
+++ app/helpers/spree/application_helper_decorator.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
   rescue
     '#'
   end
+  
+      def get_option_values(option_type)
+        list = Spree::OptionValue.joins(:option_type)
+        list = list.where('spree_option_types.name = ?', option_type).map &:presentation        
+      end
 end
