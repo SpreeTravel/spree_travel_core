bab0117063f9378915eead467b6b2077996b9862
Pedro Quintero Rojas <pqr@uh.cu>
2014-01-17 09:06:27 -0500

    ya el helper este está en su lugar

---
+++ app/helpers/spree/base_helper_decorator.rb
@@ -0,0 +1,13 @@
+module Spree
+
+  BaseHelper.class_eval do
+
+    def get_option_values(option_type)
+      list = Spree::OptionValue.joins(:option_type)
+      list = list.where('spree_option_types.name = ?', option_type)
+      list.map {|l| [l.presentation, l.id]}
+    end
+
+  end
+
+end
