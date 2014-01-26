36bf33bdb093744cbc999a01df82f8c4ee3c8ca3
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-01-26 01:03:40 -0500

    automatic commit to pull changes from github

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
 
   post 'products/get_ajax_price'
+  
+#  patch 'line_items/:id/update'
+  patch 'line_items/update/:id' => 'line_items#update', :as => 'line_items_update'
+
+  
+  resources :paxes
+  
 end
