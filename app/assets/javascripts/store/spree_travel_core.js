314510998a2d63d39d3e5738e44e6a1055087701
Cesar Lage <kaerdsar@gmail.com>
2014-02-08 02:17:11 -0500

    casi lista la busqueda de precio desde la peticion ajax

--- app/assets/javascripts/store/spree_travel_core.js
+++ app/assets/javascripts/store/spree_travel_core.js
@@ -7,12 +7,20 @@
 function update_prices() {
    list = $('.ajax-price');
     $.each(list, function(index, object) {
-        object = $(object);   
+        object = $(object);
         object.html('<img src="/assets/ajax-loader.gif" >');
         product_id = object.attr('data-product-hook');
         $.ajax({
           data_type: 'JSON',
-          data: {product_id: product_id},
+          data: {
+	      product_id: product_id,
+	      start_date: document.getElementById('start_date').value,
+	      end_date: document.getElementById('end_date').value,
+	      adult: document.getElementById('adult').value,
+	      child: document.getElementById('child').value,
+	      room: document.getElementById('room').value,
+	      plan: document.getElementById('plan').value,
+	  },
           type: 'POST',
           url: '/products/get_ajax_price',
           success: function (result) {
--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -3,11 +3,14 @@ module Spree
 
     def get_ajax_price
       product = Spree::Product.find(params[:product_id])
-      string = product.price
+      prices = product.calculator_class.calculate_price(params).to_s
+      # TODO: llegan dos variantes en vez de una, la lista de precios no se
+      # muestra bien en la vista
+      puts '-------------------------------> ' + prices
       respond_to do |format|
         number = rand(4)
         sleep number
-        format.json {render :text => string }
+        format.json {render :text => prices }
       end
     end
 
