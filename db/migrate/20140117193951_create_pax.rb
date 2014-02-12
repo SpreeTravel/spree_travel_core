b25e237c80d368aeb2dd5907ab439a029b595792
Cesar Lage <kaerdsar@gmail.com>
2014-02-12 12:26:23 -0500

    agregando campo line_item_id en spree_paxes

--- db/migrate/20140117193951_create_pax.rb
+++ db/migrate/20140117193951_create_pax.rb
@@ -2,8 +2,8 @@ class CreatePax < ActiveRecord::Migration
   def change
     create_table :spree_paxes do |t|
       t.string :first_name
-      t.string, :last_name
-      t.string, :sex
+      t.string :last_name
+      t.string :sex
       t.date :birth
     end
   end
