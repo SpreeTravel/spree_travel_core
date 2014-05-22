766fdf7c311d4e05f1ade7c694f328e767694be5
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-05-22 12:43:31 -0400

    a hotel can be added to the car

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -7,9 +7,7 @@ module Spree
       prices = calculator_class.calculate_price(params).sort
       #TODO este método de calcular la variante se llama dos veces
       variant = calculator_class.calculate_variant(params)
-      puts "==========================================================="
-      puts variant.inspect
-      prices = [prices[0], ' ... ', prices[-1]] if prices.count > 1
+      prices = "#{prices[0]} ... #{prices[-1]}" if prices.count > 1
       hash = { :variant => variant.id, :prices => prices.to_s }
       respond_to do |format|
         number = rand(3)
