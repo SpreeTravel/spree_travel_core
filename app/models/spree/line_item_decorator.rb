aa0b9a3b7d33f6be854956742f4e7ec5b9dfdec0
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-05-26 16:33:34 -0400

    adding line_itme relation with context

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,6 +1,8 @@
 module Spree
-  LineItem.class_eval do 
+  LineItem.class_eval do
     has_many :paxes, :class_name => "Spree::Pax", :dependent => :destroy
+    has_one :context, :class_name => "Spree::Context"
+
     accepts_nested_attributes_for :paxes
-  end  
+  end
 end
