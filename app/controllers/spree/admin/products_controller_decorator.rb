063dacc3ba996bf90cc0a0f17aa841e2deb0d4a6
Pedro Quintero Rojas <pqr@uh.cu>
2014-10-08 18:46:09 -0400

    defining method: generate_variant

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -5,15 +5,7 @@ module Spree
 
       #NOTE: Esto asume que un solo option_type define a la variante
       def after_save
-        product_type = @product.product_type
-        product_type.variant_option_types.each do |ot|
-          ot.option_values.each do |ov|
-            sku = ot.name.downcase.split() * '_' + '_' + ov.presentation.downcase.split() * '_'
-            variant = Spree::Variant.new(sku: sku, price: 0)
-            variant.option_values << ov
-            @product.variants << variant
-          end
-        end
+        @product.generate_variants
       end
     end
   end
