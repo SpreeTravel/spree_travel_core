d33511ffd659c8402c7c183c0319632a139c9128
Alejandro Dustet <aledustet@gmail.com>
2014-11-04 15:10:26 -0500

    Added some specs and fixed products list only show available products checkbox logic

--- lib/spree_travel_core/factories/option_value_factory.rb
+++ lib/spree_travel_core/factories/option_value_factory.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do
   factory :option_value_decorated, class: Spree::OptionValue do
     option_type_decorated
-    sequence(:name) { |n|
-      presentation = "Option Value ##{n}"
-      "product_type_##{n}"
-    }
+    sequence(:name) { |n| "product_type_##{n}" }
+    sequence(:presentation} { |n| "Option Value ##{n}" }
     enabled true
   end
 end
--- spec/models/pax_spec.rb
+++ spec/models/pax_spec.rb
@@ -3,17 +3,24 @@ require 'spec_helper'
 describe Spree::Pax do
 
   it 'haves a valid default factory' do
-    expect(build(:pax)).to be_valid
+    expect(build(:male_pax)).to be_valid
   end
 
   it 'is not valid without a first name' do
-    expect(build(:pax, first_name: nil)).not_to be_valid
+    expect(build(:male_pax, first_name: nil)).not_to be_valid
   end
 
   it 'is not valid without a last name' do
-    expect(build(:pax, last_name: nil)).not_to be_valid
+    expect(build(:male_pax, last_name: nil)).not_to be_valid
   end
 
-  #TODO Create specs for only two sexes
+  it 'is not valid without a sex' do
+    expect(build(:male_pax, sex: nil)).not_to be_valid
+  end
 
+  it 'only accept sex values for male and female' do
+    expect(build(:male_pax)).to be_valid
+    expect(build(:female_pax)).to be_valid
+    expect(build(:pax, sex: "other")).not_to be_valid
+  end
 end
