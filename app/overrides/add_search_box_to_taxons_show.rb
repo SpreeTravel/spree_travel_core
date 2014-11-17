e017f8fffc94711d48fb8f54a10fc43488ff5597
Pedro Quintero Rojas <pqr@uh.cu>
2014-11-17 08:01:08 -0500

    replacing search_box_taxon for generic search_box

--- app/overrides/add_search_box_to_taxons_show.rb
+++ app/overrides/add_search_box_to_taxons_show.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
     :virtual_path => 'spree/taxons/show',
     :name => 'add_search_box_to_taxons_show',
     :insert_before => "[data-hook='taxon_products']",
-    :partial => "spree/taxons/search_box_taxon",
+    :partial => "spree/shared/search_box",
     :disabled => true
 )
