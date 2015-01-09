5ef3cf72f9d6287f0e0c9a037ec5c9f8ace0dcc4
Alejandro Dustet <aledustet@gmail.com>
2015-01-09 00:08:46 -0500

    Added one more test case, struggling with missing methods for testing

--- spec/views/spree/admin/create_product_view_spec.rb
+++ spec/views/spree/admin/create_product_view_spec.rb
@@ -1,22 +1,16 @@
 require 'spec_helper'
 
-describe "Spree Product Create View" do
+describe "Spree Home View" do
   context 'as Admin user' do
     #spree helper to enter as admin
     before(:each) do
-      user = create(:admin_user, email: 'admin@person.com')
-      visit spree.admin_path
-
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: 'secret'
-      click_button 'Login'
+      visit spree.home_path
     end
 
     it 'haves the fields to add a product type' do
-      visit spree.admin_path
-      click_link "Products"
-      click_link("New Product")
-      expect(page).to have_field("product_type")
+      within("#{}cart_form") do
+         expect(page).to have_content('Add to Cart')
+      end
     end
   end
 end
