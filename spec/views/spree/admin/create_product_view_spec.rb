413ac82621e75a500a3b1a960496bce741b9983a
Alejandro Dustet <aledustet@gmail.com>
2015-01-09 00:09:40 -0500

    Oops switched places of files

--- spec/views/spree/admin/create_product_view_spec.rb
+++ spec/views/spree/admin/create_product_view_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
-describe "Spree Home View" do
+describe "Spree Product Create View" do
   context 'as Admin user' do
     #spree helper to enter as admin
     before(:each) do
-      visit spree.home_path
+      user = create(:admin_user, email: 'admin@person.com')
+      visit spree.admin_path
+
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'secret'
+      click_button 'Login'
     end
 
     it 'haves the fields to add a product type' do
-      within("#{}cart_form") do
-         expect(page).to have_content('Add to Cart')
-      end
+      visit spree.admin_path
+      click_link "Products"
+      click_link("New Product")
+      expect(page).to have_field("product_type")
     end
   end
 end
