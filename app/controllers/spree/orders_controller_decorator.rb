0f0f691b08348d2162976b9cdae60e7f355f2359
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2015-08-25 14:57:50 -0400

    sustituin the variant for the rates when going to checkout

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -4,6 +4,7 @@ module Spree
       def populate
         order    = current_order(create_order_if_necessary: true)
         variant  = Spree::Variant.find(params[:variant_id])
+        rate  = Spree::Rate.find(params[:rate_id])
         context = Spree::Context.build_from_params(params, :temporal => false)
         quantity = params[:quantity].to_i
         options  = params[:options] || {}
@@ -11,7 +12,7 @@ module Spree
         #Only one service at a time
         if quantity.between?(1, 1)
           begin
-            order.contents.add(variant, context, quantity, options)
+            order.contents.add(rate, context, quantity, options)
           rescue ActiveRecord::RecordInvalid => e
             error = e.record.errors.full_messages.join(", ")
           end
@@ -23,14 +24,14 @@ module Spree
           flash[:error] = error
           redirect_back_or_default(spree.root_path)
         else
-          if true #TODO Yes-No-Cart Preference
-            @order = current_order
-            params.merge!('checkout'=>'')
-            self.update
-          else
+          if Spree::Config.use_cart
             respond_with(order) do |format|
               format.html { redirect_to cart_path }
             end
+          else
+            @order = current_order
+            params.merge!('checkout'=>'')
+            self.update
           end
         end
       end
--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,40 +1,61 @@
 module Spree
   OrderContents.class_eval do
 
-    def add(variant, context, quantity = 1, options = {})
+    def add(rate, context, quantity = 1, options = {})
       timestamp = Time.now
-      line_item = add_to_line_item(variant, context, quantity, options)
+      line_item = add_to_line_item(rate, context, quantity, options)
       options[:line_item_created] = true if timestamp <= line_item.created_at
       after_add_or_remove(line_item, options)
     end
 
     private
 
-    def add_to_line_item(variant, context, quantity, options = {})
-      line_item = grab_line_item_by_variant(variant, context, false, options)
+    def add_to_line_item(rate, context, quantity, options = {})
+      line_item = grab_line_item_by_variant(rate, context, false, options)
 
       opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
                                                     permit(Spree::PermittedAttributes.line_item_attributes)
 
-      if true #TODO here must go the Yes-No-Cart Preference
-        if line_item
-          line_item.destroy
-        end
-        line_item = order.line_items.new(quantity: quantity, variant: variant, options: opts)
-        line_item.context = context
-      else
+      if Spree::Config.use_cart
         if line_item
           line_item.quantity += quantity.to_i
           line_item.currency = currency unless currency.nil?
           line_item.context = context
         else
-          line_item = order.line_items.new(quantity: quantity, variant: variant, options: opts)
+          line_item = order.line_items.new(quantity: quantity, variant: rate.variant, options: opts)
+          line_item.price = get_rate_price(rate, context.adult(temporal:false), context.child(temporal:false))
+          line_item.cost_price = rate.variant.cost_price if line_item.cost_price.nil?
+          line_item.currency = rate.variant.currency if line_item.currency.nil?
         end
+      else
+        if line_item
+          line_item.destroy
+        end
+        line_item = order.line_items.new(quantity: quantity, variant: rate.variant, options: opts)
+        line_item.context = context
+        line_item.price = get_rate_price(rate, context.adult(temporal:false), context.child(temporal:false))
+        line_item.cost_price = rate.variant.cost_price if line_item.cost_price.nil?
+        line_item.currency = rate.variant.currency if line_item.currency.nil?
       end
       line_item.target_shipment = options[:shipment] if options.has_key? :shipment
       line_item.save!
       line_item
     end
+
+    private
+
+    def get_rate_price(rate, adults, children)
+      adults = adults.to_i
+      children = children.to_i
+      adults_hash = {1 => 'simple', 2 => 'double', 3 => 'triple'}
+      price = adults * rate.send(adults_hash[adults]).to_f
+      price += rate.first_child.to_f if children >= 1
+      price += rate.second_child.to_f if children == 2
+      price
+    end
+
+
+
 
   end
 end
