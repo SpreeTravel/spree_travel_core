f6a941dc120b66a1148d87fb29bcebc65312d139
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2015-08-25 15:13:21 -0400

    two helper method and a empty methos, look at the comment

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -7,24 +7,10 @@ module Spree
 
 
     def copy_price
-      # if variant
-      #
-      #   combinations = product.calculate_price(context, :temporal => false )
-      #
-      #   match = []
-      #   combinations = combinations.where(variant_id: variant.id)
-      #   combinations.each do |c|
-      #     if c.variant.option_values.first == variant.option_values.first
-      #       match << c
-      #     end
-      #   end
-      #
-      #   price = match.first[:price] * (context.end_date(temporal:false).to_date - context.start_date(temporal:false).to_date).to_i * context.rooms(temporal:false).to_i
-      #
-      #   self.price = price
-      #   self.cost_price = variant.cost_price if cost_price.nil?
-      #   self.currency = variant.currency if currency.nil?
-      # end
+    #   TODO look into this method, is empty because the logic is in
+    #   'add_to_line_item' in the OrderContentDecorator
+    #   i could not pass the 'rate' to this method
+
     end
 
 
