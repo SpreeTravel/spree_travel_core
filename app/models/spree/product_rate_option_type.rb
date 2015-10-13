e87a77992d6421d639634d1f9f6212678bab5526
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2015-10-12 21:00:28 -0400

    Delete product_rate_option_type.rb

--- app/models/spree/product_rate_option_type.rb
+++
@@ -1,6 +0,0 @@
-module Spree
-  class ProductRateOptionType < ActiveRecord::Base
-    belongs_to :product, :class_name => 'Spree::Product', :foreign_key => 'product_id'
-    belongs_to :rate_option_type, :class_name => 'Spree::OptionType', :foreign_key => 'option_type_id'
-  end
-end
