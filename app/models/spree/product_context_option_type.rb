f5b33574943dee736e87ef708de524157115e92d
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2015-10-12 20:48:38 -0400

    Delete product_context_option_type.rb

--- app/models/spree/product_context_option_type.rb
+++
@@ -1,6 +0,0 @@
-module Spree
-  class ProductContextOptionType < ActiveRecord::Base
-    belongs_to :product, :class_name => 'Spree::Product', :foreign_key => 'product_id'
-    belongs_to :context_option_type, :class_name => 'Spree::OptionType', :foreign_key => 'option_type_id'
-  end
-end
