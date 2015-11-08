2c873981c8221171d285f48efeea57fc84b7c853
Miguel Sancho <sanchojaf@gmail.com>
2015-11-08 14:17:12 -0500

    set by default use_care preference variable like false

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,4 +1,4 @@
 Spree::AppConfiguration.class_eval do
   #if you want to have a cart or directly to the checkout process
-  preference :use_cart, :boolean, default: true 
+  preference :use_cart, :boolean, default: false 
 end
