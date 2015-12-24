4adbd6909dc5094558fa8a012ea0988d4f3e1864
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2015-12-23 23:25:40 -0500

    including datepicker as default

--- app/assets/javascripts/store/datepicker.js
+++ app/assets/javascripts/store/datepicker.js
@@ -1,19 +1,11 @@
 
 
-//$(function(){
-//    $(".datepicker").datepicker({
-//        dateFormat: 'yy-mm-dd',
-//        minDate: 0
-//    })
-//});
-
-//$(function(){
-//    $(".birthdatepicker").datepicker({
-//        dateFormat: 'yy-mm-dd',
-//        changeMonth: true,
-//        changeYear: true,
-//        yearRange: "1922:#{Date::today.year()}"
-//    })
-//});
+$(function(){
+    $(".datepicker").datepicker({
+        dateFormat: 'yy-mm-dd',
+        minDate: 0,
+        numberOfMonths: 2
+    })
+});
 
 
