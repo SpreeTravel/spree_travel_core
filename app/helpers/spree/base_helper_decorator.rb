eb606a02b10c48de280e6f4d9f7eba9f29238011
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2016-01-27 16:57:11 -0500

    change the name of the display price helper function

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -3,9 +3,9 @@ Spree::BaseHelper.class_eval do
     # def display_price(product_or_variant)
     #   product_or_variant.price_in(current_currency).display_price.to_html
     # end
-    #
-    #TODO hay que tener en cuenta aqui la tasa de cambio como está en el método de arriba
-    def display_the_price(product_or_variant)
+
+    #TODO must take into account the currency, beacuse here i am not having any currency into account
+    def display_travel_price(product_or_variant)
       if product_or_variant.class == Spree::Product
         context = Spree::Context.build_from_params(params.merge!(product_type: product_or_variant.product_type.name), :temporal => true)
         product_or_variant.calculate_price(context, product_or_variant.variants.first, :temporal => true )
