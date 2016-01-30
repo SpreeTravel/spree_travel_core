9f233090e6af57cecb6c5a4f59f860b0122c27f1
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2016-01-30 10:10:11 -0500

    removing to be compatible with original spree

--- app/overrides/remove_variant_edit_left_column.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/variants/_form",
-                     :name => "remove_variant_edit_left_column",
-                     :remove => "[data-hook='admin_variant_form_additional_fields']",
-                     :disabled => false
-)
