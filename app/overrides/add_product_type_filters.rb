5d3839b8e5e15c90606acaf04e844570ed5b6f29
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2016-08-08 16:03:31 -0400

    override to filter in the admin interface via product type

---
+++ app/overrides/add_product_type_filters.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+    :virtual_path => 'spree/admin/products/index',
+    :name => 'add_product_type_filters',
+    :insert_bottom => "[data-hook='admin_products_index_search']",
+    :partial => 'spree/admin/products/product_type_filter'
+)
