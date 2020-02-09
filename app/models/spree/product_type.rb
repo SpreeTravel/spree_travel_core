8173f6a168a63713fd53369b00ac4e22d272d458
Raul <raul@backlotcars.com>
2020-02-09 17:55:09 -0300

    minor ruby code style improvements

--- app/models/spree/product_type.rb
+++ app/models/spree/product_type.rb
@@ -2,23 +2,23 @@ module Spree
   class ProductType < Spree::Base
 
     has_and_belongs_to_many :rate_option_types,
-      :join_table => :spree_product_type_rate_option_types,
-      :class_name => 'Spree::OptionType',
-      :foreign_key => 'product_type_id',
-      :association_foreign_key => 'rate_option_type_id'
+      join_table: :spree_product_type_rate_option_types,
+      class_name: 'Spree::OptionType',
+      foreign_key:'product_type_id',
+      association_foreign_key:'rate_option_type_id'
     has_and_belongs_to_many :context_option_types,
-      :join_table => :spree_product_type_context_option_types,
-      :class_name => 'Spree::OptionType',
-      :foreign_key => 'product_type_id',
-      :association_foreign_key => 'context_option_type_id'
+      join_table: :spree_product_type_context_option_types,
+      class_name: 'Spree::OptionType',
+      foreign_key:'product_type_id',
+      association_foreign_key:'context_option_type_id'
     has_and_belongs_to_many :variant_option_types,
-      :join_table => :spree_product_type_variant_option_types,
-      :class_name => 'Spree::OptionType',
-      :foreign_key => 'product_type_id',
-      :association_foreign_key => 'variant_option_type_id'
+      join_table: :spree_product_type_variant_option_types,
+      class_name: 'Spree::OptionType',
+      foreign_key:'product_type_id',
+      association_foreign_key:'variant_option_type_id'
     has_one :calculator,
-      :class_name => 'Spree::TravelCalculator',
-      :foreign_key => 'product_type_id'
+      class_name: 'Spree::TravelCalculator',
+      foreign_key:'product_type_id'
 
     validates_presence_of :name, :presentation
     validates_uniqueness_of :name
@@ -30,14 +30,11 @@ module Spree
       else
         (context_option_types).uniq
       end
-
-
     end
 
     # Modified enabled definition, to exclude product product_type, for lacking of relevance
     def self.enabled
-      where(:enabled => true)
+      where(enabled: true)
     end
-
   end
 end
