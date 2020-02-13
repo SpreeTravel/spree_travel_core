f4ca08e2959889890cb19b63780d3bd4be1475b3
Raul <raul@backlotcars.com>
2020-02-13 17:26:24 -0300

    adding field validation

--- app/models/spree/travel_calculator.rb
+++ app/models/spree/travel_calculator.rb
@@ -1,5 +1,7 @@
 module Spree
   class TravelCalculator < Spree::Base
     belongs_to :product_type, class_name: 'Spree::ProductType', foreign_key: 'product_type_id'
+
+    validates_presence_of :name
   end
 end
