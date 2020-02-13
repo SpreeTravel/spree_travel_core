fb46148d4cea5969d78bca468be5adbd976dc8a6
Raul <raul@backlotcars.com>
2020-02-13 17:25:55 -0300

    Why: We need to use a new Base Search only for SpreeTravel not to affect the base Spree class Change: Added a new function for the SpreeTravel searcher

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,11 +1,17 @@
+require 'spree/core/search/spree_travel_base'
+
 module Spree::AppConfigurationDecorator
   def self.prepended(base)
     base.preference :use_cart, :boolean, default: true #true if you want to have a cart or false will directly to the checkout process with one product only
   end
 
   # searcher_class allows spree extension writers to provide their own Search class
-  def searcher_class
-    @searcher_class ||= Spree::Core::Search::SpreeTravelBase
+  def searcher_travel_class
+    @searcher_travel_class ||= Spree::Core::Search::SpreeTravelBase
   end
 
+  attr_writer :searcher_travel_class
+
 end
+
+Spree::AppConfiguration.prepend Spree::AppConfigurationDecorator
