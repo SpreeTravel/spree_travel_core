8fdb87b0b1db9662b87339cc897e11f75d9a8c78
Raul <raul@backlotcars.com>
2020-02-13 17:31:21 -0300

    Why: Travel Products may not be accesible via the url because they need a context, with out a context they may not be showable Change: Override the product show method in order to redirect if someone tres to navigate via url to a product.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -3,13 +3,29 @@ module Spree::ProductsControllerDecorator
     base.before_action :add_childrens_param, only: :show
   end
 
-    def add_childrens_param
-      begin
-        @childrens = @product.childrens || @product.variants
-      rescue
-        @childrens = @product.variants
-      end
+  def show
+    if @product.product_type.present? && params[:product_type].nil?
+      flash[:error] = "You don't have access to this product"
+      redirect_to root_path
+    else
+      @variants = @product.variants_including_master.
+          spree_base_scopes.
+          active(current_currency).
+          includes([:option_values, :images])
+      @product_properties = @product.product_properties.includes(:property)
+      @taxon = params[:taxon_id].present? ? Spree::Taxon.find(params[:taxon_id]) : @product.taxons.first
+      redirect_if_legacy_path
     end
+
+  end
+
+  def add_childrens_param
+    begin
+      @childrens = @product.childrens || @product.variants
+    rescue
+      @childrens = @product.variants
+    end
+  end
 end
 
 
