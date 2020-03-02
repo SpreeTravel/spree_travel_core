d7224d38872f3e093d8488b03f4d83df76191e92
Raul <raul@backlotcars.com>
2020-03-02 19:18:31 -0300

    Why: This methods where only used when creating the rates Change: This methos where migrated into the spree_travel_sample gem

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -31,24 +31,6 @@ module Spree::ProductDecorator
     calculator_instance.calculate_price(context, self, variant, options)
   end
 
-  def generate_variants
-    variations.each do |array|
-      variant = Spree::Variant.new
-      variant.sku = FFaker.bothify('???-######').upcase
-      variant.price = 0
-      variant.product_id = self.id
-      variant.calculator = self.product_type.calculator
-      string = "PRODUCT:" + " #{self.name}: "
-      for ov in array
-        opt_name = ov.option_type.name
-        opt_value = ov.name
-        string += "#{opt_name.upcase}: #{opt_value}, "
-        variant.set_option_value(opt_name, opt_value)
-      end
-      variant.save
-    end
-  end
-
   def destination_taxon
     destination_taxonomy = Spree::Taxonomy.where(:name => 'Destination').first
     self.taxons.where(:taxonomy_id => destination_taxonomy.id).first
@@ -60,47 +42,11 @@ module Spree::ProductDecorator
     destination_taxon.name rescue "no place"
   end
 
-  # TODO: poner bonito la seleccion de variantes en la creacion de
-  # productos, parece que es de Spree
-
-  def variations
-    the_array = []
-    recursive_variations(self.option_types, the_array)
-    the_array
-  end
-
-  def recursive_variations(the_option_types, the_big_array, index = 0, array = [])
-    if the_option_types.length == index
-      the_big_array << array.clone
-    else
-      for option_value in the_option_types[index].option_values
-        array[index] = option_value
-        recursive_variations(the_option_types, the_big_array, index + 1, array)
-      end
-    end
-  end
-
   private
+
   def self.ransackable_scopes(auth_object = nil)
     [:product_type_id]
   end
-
-
-  # def self.with_price(context)
-  #   product_type = Spree::ProductType.find_by_name(context.product_type)
-  #   string = calculator_instance_for(product_type).combination_string_for_search(context) if product_type
-  #   list = Spree::Product.where('1 > 0')
-  #   list = list.where(:product_type_id => product_type.id) if product_type
-  #   list = list.joins(:combinations)
-  #   list = list.where('spree_combinations.start_date <= ?', context.start_date) if context.start_date.present?
-  #   list = list.where('spree_combinations.end_date >= ?', context.end_date) if context.end_date.present?
-  #   list = list.where('spree_combinations.adults' => context.adult) if context.adult.present?
-  #   list = list.where('spree_combinations.children' => context.child) if context.child.present?
-  #   list = list.where('spree_combinations.other like ?', string) if product_type && string
-  #   list = list.uniq
-  #   list
-  # end
-
 end
 
 Spree::Product.prepend Spree::ProductDecorator
