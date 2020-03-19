79ced5a86f2659005ef2230f00d7e4b0f9a81298
Raul <raul@backlotcars.com>
2020-03-19 00:01:04 -0300

    Indenting file

--- app/services/spree/add_item_decorator.rb
+++ app/services/spree/add_item_decorator.rb
@@ -1,47 +1,47 @@
-    module Spree::AddItemDecorator
-      prepend Spree::ServiceModule::Base
+module Spree::AddItemDecorator
+  prepend Spree::ServiceModule::Base
 
-      def call(order:, variant:, context:, rate: nil, quantity: nil, options: {})
-        ApplicationRecord.transaction do
-          run :add_to_line_item
-          run Spree::Dependencies.cart_recalculate_service.constantize
-        end
-      end
-
-      private
-
-      def add_to_line_item(order:, variant:, context:, rate: nil, quantity: nil, options: {})
-        # we used to have here a use_cart configuration to have cars or not, can be seen on brnach 3-0-stable
-        options ||= {}
-        quantity ||= 1
-
-        line_item = Spree::Dependencies.line_item_by_variant_finder.constantize.new.execute(order: order, variant: variant, options: options)
-
-        line_item_created = line_item.nil?
-        if line_item.nil?
-          opts = ::Spree::PermittedAttributes.line_item_attributes.flatten.each_with_object({}) do |attribute, result|
-            result[attribute] = options[attribute]
-          end.merge(currency: order.currency).delete_if { |_key, value| value.nil? }
-
-          line_item = order.line_items.new(quantity: quantity,
-                                           variant: variant,
-                                           context: context,
-                                           rate: rate,
-                                           options: opts)
-        else
-          line_item.quantity += quantity.to_i
-        end
-
-        line_item.target_shipment = options[:shipment] if options.key? :shipment
-
-        return failure(line_item) unless line_item.save
-
-        line_item.reload.update_price
-
-        ::Spree::TaxRate.adjust(order, [line_item]) if line_item_created
-        success(order: order, line_item: line_item, line_item_created: line_item_created, options: options)
-      end
+  def call(order:, variant:, context:, rate: nil, quantity: nil, options: {})
+    ApplicationRecord.transaction do
+      run :add_to_line_item
+      run Spree::Dependencies.cart_recalculate_service.constantize
     end
+  end
+
+  private
+
+  def add_to_line_item(order:, variant:, context:, rate: nil, quantity: nil, options: {})
+    # we used to have here a use_cart configuration to have cars or not, can be seen on brnach 3-0-stable
+    options ||= {}
+    quantity ||= 1
+
+    line_item = Spree::Dependencies.line_item_by_variant_finder.constantize.new.execute(order: order, variant: variant, options: options)
+
+    line_item_created = line_item.nil?
+    if line_item.nil?
+      opts = ::Spree::PermittedAttributes.line_item_attributes.flatten.each_with_object({}) do |attribute, result|
+        result[attribute] = options[attribute]
+      end.merge(currency: order.currency).delete_if { |_key, value| value.nil? }
+
+      line_item = order.line_items.new(quantity: quantity,
+                                       variant: variant,
+                                       context: context,
+                                       rate: rate,
+                                       options: opts)
+    else
+      line_item.quantity += quantity.to_i
+    end
+
+    line_item.target_shipment = options[:shipment] if options.key? :shipment
+
+    return failure(line_item) unless line_item.save
+
+    line_item.reload.update_price
+
+    ::Spree::TaxRate.adjust(order, [line_item]) if line_item_created
+    success(order: order, line_item: line_item, line_item_created: line_item_created, options: options)
+  end
+end
 
 
 Spree::Cart::AddItem.prepend Spree::AddItemDecorator
