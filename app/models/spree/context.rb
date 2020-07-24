fc8d971a7ea5f1059adde501713d0dc7d31ba014
Raul <raul@backlotcars.com>
2020-07-24 16:53:51 -0300

    Rubocoped

--- app/models/spree/context.rb
+++ app/models/spree/context.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 module Spree
   class Context < Spree::Base
-
     include PersistedDynamicAttribute
     include TemporalDynamicAttribute
 
     has_many :line_items, class_name: 'Spree::LineItem',
-                          foreign_key:'context_id'
+                          foreign_key: 'context_id'
     has_many :option_values, class_name: 'Spree::ContextOptionValue',
                              foreign_key: 'context_id',
                              dependent: :destroy
@@ -37,65 +38,65 @@ module Spree
       return context
     end
 
-    def product_type(options = {temporal: true})
+    def product_type(options = { temporal: true })
       get_mixed_option_value(:product_type, options)
     end
 
-    def start_date(options = {temporal: true})
+    def start_date(options = { temporal: true })
       get_mixed_option_value(:start_date, options)
     end
 
-    def end_date(options = {temporal: true})
+    def end_date(options = { temporal: true })
       get_mixed_option_value(:end_date, options)
     end
 
-    def plan(options = {temporal: true})
+    def plan(options = { temporal: true })
       get_mixed_option_value(:plan, options)
     end
 
-    def adult(options = {temporal: true})
+    def adult(options = { temporal: true })
       get_mixed_option_value(:adult, options)
     end
 
-    def child(options = {temporal: true})
+    def child(options = { temporal: true })
       get_mixed_option_value(:child, options)
     end
 
-    #this is for the amount of rooms
-    def room_count(options = {temporal: true})
+    # this is for the amount of rooms
+    def room_count(options = { temporal: true })
       get_mixed_option_value(:room_count, options)
     end
 
-    def cabin_count(options = {temporal: true})
+    def cabin_count(options = { temporal: true })
       get_mixed_option_value(:cabin_count, options)
     end
 
-    #this is for the room type (Sweet, Junio Sweet, etc.....)
-    def room(options = {temporal: true})
+    # this is for the room type (Sweet, Junio Sweet, etc.....)
+    def room(options = { temporal: true })
       get_mixed_option_value(:room, options)
     end
 
-    def departure_date(options = {temporal: true})
+    def departure_date(options = { temporal: true })
       get_mixed_option_value(:departure_date, options)
     end
 
-    def category(options = {temporal: true})
+    def category(options = { temporal: true })
       get_mixed_option_value(:category, options)
     end
 
-    def pickup_destination(options = {temporal: true})
+    def pickup_destination(options = { temporal: true })
       get_mixed_option_value(:pickup_destination, options)
-    end   
-    
-    def return_destination(options = {temporal: true})
+    end
+
+    def return_destination(options = { temporal: true })
       get_mixed_option_value(:return_destination, options)
     end
 
-    def pickup_date(options = {temporal: true})
+    def pickup_date(options = { temporal: true })
       get_mixed_option_value(:pickup_date, options)
     end
 
-    def return_date(options = {temporal: true})
+    def return_date(options = { temporal: true })
       get_mixed_option_value(:return_date, options)
     end
 
