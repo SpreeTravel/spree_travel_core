f87c6489e87d067b2a571ed4527490cb4d3a6f71
Raul <raul@backlotcars.com>
2021-01-11 07:40:17 -0300

    Why: Code improvement. common operation where united

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -18,20 +18,17 @@ module Spree::LineItemDecorator
 
   def copy_price
     if variant
-      if variant.product.product_type.present?
+      if variant.product_type.present?
         # TODO: take into account here the currency change, i am not taking it now
         # TODO: this has to be improved, regarding the comparinson with the rate.
         variant.product.calculate_price(context, variant, temporal:false).each do |hash|
           self.price = hash[:price].match(/(\d.+)/)[1].gsub(',','').to_f if self.rate.id == hash[:rate]
         end
-        # self.price = variant.product.calculate_price(context, variant, temporal:false).first[:price]
-        self.cost_price = variant.cost_price if cost_price.nil?
-        self.currency = variant.currency if currency.nil?
       else
         update_price if price.nil?
-        self.cost_price = variant.cost_price if cost_price.nil?
-        self.currency = variant.currency if currency.nil?
       end
+      self.cost_price = variant.cost_price if cost_price.nil?
+      self.currency = variant.currency if currency.nil?
     end
   end
 end
