0bff663236186595629eef05909b1abc13d5a678
Raul <raul@backlotcars.com>
2021-01-15 07:41:20 -0300

    Why: methods where not used

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -31,45 +31,6 @@ module Spree::VariantDecorator
       self.currency = Spree::Config[:currency]
     end
   end
-
-  def count_on_hand
-    byebug
-    100
-  end
 end
 
 Spree::Variant.prepend Spree::VariantDecorator
-
-module Spree::VariantDecoratorClassMethod
-  # NOTE: esto asume que todos los option types de una variante son
-  # de tipo selection
-  def variant_from_params(params)
-    byebug
-    pt = params['product_type']
-    return nil unless pt
-    product_id = params['product_id']
-    return nil unless product_id
-
-    product = Spree::Product.find(product_id)
-    list = product.variants.joins(:option_values => :option_type)
-    Spree::Product.find(product_id).variants.each do |var|
-      puts var.option_values
-    end
-    product_type = Spree::ProductType.find_by_name(pt)
-    product_type.variant_option_types.each do |ot|
-      ov = params[pt + '_' + ot.name] || params[ot.name]
-      return nil unless ov
-      list = list.where('spree_option_types.name = ? and spree_option_values.id = ?', ot.name, ov)
-    end
-    case list.count
-    when 0
-      product.master
-    when 1
-      list.first
-    else
-      raise Exception.new("Revisa, que hay bateo en los datos")
-    end
-  end
-end
-
-Spree::Variant.singleton_class.send :prepend, Spree::VariantDecoratorClassMethod
