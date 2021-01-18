3d3b4e3b1fa861b0b0cfd570619a88513393f859
Raul <raul@backlotcars.com>
2021-01-18 07:10:19 -0300

    Why: Dynamic functions to get the values of contexts and rates was not covered by test

--- spec/models/spree/rate_spec.rb
+++ spec/models/spree/rate_spec.rb
@@ -10,12 +10,21 @@ describe Spree::Rate do
     expect(build(:rate)).to be_valid
   end
 
-  # it 'respond to the attributes' do
-  #   create(:product_type, :with_rate_option_types)
-  #   rate = Spree::Rate.new.date_option_type_a
-  #   byebug
-  #   # byebug
-  #   expect(rate.respond_to?(:date_option_type_a)).to eq true
-  # end
+  it 'should respond to the start_date' do
+    allow_any_instance_of(Spree::Rate).to receive(:get_persisted_option_value).with(:start_date)
+    Spree::Rate.new.start_date
+  end
 
+  it 'should respond to the end_date' do
+    allow_any_instance_of(Spree::Rate).to receive(:get_persisted_option_value).with(:end_date)
+    Spree::Rate.new.end_date
+  end
+
+  # This method are from hotel gem, will stay here until migration
+  %i[plan simple double triple first_child second_child one_adult].each do |method_name|
+    it "should respond to the #{method_name}" do
+      allow_any_instance_of(Spree::Rate).to receive(:get_persisted_option_value).with(method_name)
+      Spree::Rate.new.send(method_name)
+    end
+  end
 end
