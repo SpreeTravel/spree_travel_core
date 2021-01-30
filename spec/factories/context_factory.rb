b0e34a1f9c28bdc7f4a1f80a22ca30aa4b6a88c0
Raul <raul@backlotcars.com>
2021-01-30 18:29:31 -0300

    Why: Changing the create by the build to make test a little faster

--- spec/factories/context_factory.rb
+++ spec/factories/context_factory.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :context, class: Spree::Context do
-    after(:create) do |context|
-      2.times do
-        context.context_option_values << create(:context_option_value)
-      end
+
+    after(:build) do |context|
+        context.context_option_values << build(:context_option_value)
     end
   end
 end
