2634bcec3a6c34c459d5de8c74dc75005a742e29
Raul <raul@backlotcars.com>
2021-02-24 19:08:05 -0300

    Why: The PersistedDynamicAttr module is complex and hard to test. Needed a better arquitecture to support every AttrType to know who to save and return its value, that smelled as a Strategy Design Pattern and that is the final propouse of this change. As it is right now is easy to understand, test and interact

--- app/models/spree/persisted_dynamic_attribute.rb
+++ app/models/spree/persisted_dynamic_attribute.rb
@@ -23,16 +23,7 @@ module Spree
         self.send("#{class_name.demodulize.downcase}_option_values") << context_or_rate_option_value
       end
 
-      # TODO Create a class to abstract the way each option value sets its value
-      if selection_or_destination?(option_type)
-        context_or_rate_option_value.option_value_id = value
-      elsif preciable?(option_type)
-        context_or_rate_option_value.price = value.to_i
-      else
-        context_or_rate_option_value.option_value_id = option_type.option_values.first.id
-        context_or_rate_option_value.value = value
-      end
-      context_or_rate_option_value.save
+      "Spree::#{option_type.attr_type.camelcase}OptionType".constantize.save(context_or_rate_option_value, value, option_type)
     end
 
     def get_persisted_option_value(option_type, attrib = 'presentation')
@@ -40,21 +31,7 @@ module Spree
 
       return if option_type.nil? || context_or_rate_option_value.nil?
 
-      price_or_value(context_or_rate_option_value, option_type, attrib)
-    end
-
-    def price_or_value(context_or_rate_option_value, option_type, attrib)
-      # TODO Create a class to abstract the way each option value show its value
-      if preciable?(option_type)
-        # TODO: sustitude USD by current_currency
-        context_or_rate_option_value.price_in('USD')
-                                    .display_price_including_vat_for({tax_zone: Spree::Zone.default_tax})
-                                    .money
-      elsif selection_or_destination?(context_or_rate_option_value.option_value.option_type)
-        context_or_rate_option_value.option_value.send(attrib)
-      else
-        context_or_rate_option_value.value
-      end
+      "Spree::#{option_type.attr_type.camelcase}OptionType".constantize.value(context_or_rate_option_value)
     end
 
     def get_option_type_object(option_type)
@@ -67,14 +44,6 @@ module Spree
       option_type = get_option_type_object(option_type)
 
       [option_type, find_existing_option_value(option_type)]
-    end
-
-    def selection_or_destination?(option_type)
-      %w(selection destination).include?(option_type.attr_type)
-    end
-
-    def preciable?(option_type)
-      option_type.preciable
     end
   end
 end
