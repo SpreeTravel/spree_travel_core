9eeaa3fe9e8def7e3380086ac146efa7ae1a01e3
Raul <raul@backlotcars.com>
2021-02-24 18:29:56 -0300

    Why: The name of the table thay this reference points was wrong.

--- db/migrate/20141208095810_add_calculator_to_variant.rb
+++ db/migrate/20141208095810_add_calculator_to_variant.rb
@@ -1,5 +1,5 @@
 class AddCalculatorToVariant < ActiveRecord::Migration[4.2]
   def change
-    add_reference :spree_variants, :calculator, foreign_key: {to_table: :spree_calculators}, index: true
+    add_reference :spree_variants, :calculator, foreign_key: {to_table: :spree_travel_calculators}, index: true
   end
 end
