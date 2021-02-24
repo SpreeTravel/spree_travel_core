db19e27d1967230a1f4bef3c308d15dcec02242e
Raul <raul@backlotcars.com>
2021-02-24 18:29:03 -0300

    Why: For some odd reason this two lines where removed, now they are put back

--- lib/generators/spree_travel_core/install/install_generator.rb
+++ lib/generators/spree_travel_core/install/install_generator.rb
@@ -5,10 +5,12 @@ module SpreeTravelCore
 
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_travel_core\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_travel_core\n"
       end
 
       def add_stylesheets
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_travel_core\n", before: %r{\*\/}, verbose: true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_travel_core\n", before: %r{\*\/}, verbose: true
       end
 
       def add_migrations
