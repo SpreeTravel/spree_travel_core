c08e64eb46cf2e2714192f9add1cd438187da2ed
Raul <raul@backlotcars.com>
2021-06-22 00:33:26 -0300

    Why: This column was removed

--- db/migrate/20210327121718_change_rate_option_value_data_value_column_type.rb
+++ db/migrate/20210327121718_change_rate_option_value_data_value_column_type.rb
@@ -1,6 +1,5 @@
 class ChangeRateOptionValueDataValueColumnType < ActiveRecord::Migration[6.0]
   def change
-    remove_column :spree_rate_option_values, :date_value
     add_column :spree_rate_option_values, :date_value, :date
   end
 end
